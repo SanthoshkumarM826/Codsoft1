96817297d6943fce99f981903bc4291f6db34777
SanthoshkumarM826 <msanthoshkumar169@gmail.com>
2026-02-26 11:43:44 +0530

    Create password generator.py
    Added Python password generator code

---
+++ password generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(characters) for _ in range(length))
+
+print("🔐 Password Generator 🔐")
+length = int(input("Enter password length: "))
+print("Generated Password:", generate_password(length))
